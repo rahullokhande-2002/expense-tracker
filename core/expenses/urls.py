2453cfbf771c5cf28d3da1458690a5fc38caf4dc
rahul <lokhanderahul2002@gmail.com>
2026-05-12 17:18:32 +0530

    JWT applied

--- core/expenses/urls.py
+++ core/expenses/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from .views import ExpenseViewSet
 
+
 router=DefaultRouter()
 router.register('expenses',ExpenseViewSet)
 urlpatterns =router.urls
--- core/expenses/views.py
+++ core/expenses/views.py
@@ -1,8 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Expenses
 from .serializers import ExpenseSerializers
+from rest_framework.permissions import IsAuthenticated
 
 class ExpenseViewSet(ModelViewSet):
     
     queryset=Expenses.objects.all()
     serializer_class=ExpenseSerializers
+    
+    permission_classes = [IsAuthenticated]
